0c54a4fe21f7d4392e2b47735e86d7d0f9153fa1
Pietro Magni <39495752+tatop@users.noreply.github.com>
2018-06-11 10:56:19 +0200

    Add files via upload

---
+++ arthur.py
@@ -0,0 +1,14 @@
+# /usr/bin/python
+# Created by Pietro Magni
+# Arthur Script
+# V 0.0.1 
+
+print("""      db                 mm   `7MM""")                           
+print("""     ;MM:                MM     MM""")                           
+print("""    ,V^MM.    `7Mb,od8 mmMMmm   MMpMMMb.`7MM  `7MM  `7Mb,od8""") 
+print("""   ,M  `MM      MM' "'   MM     MM    MM  MM    MM    MM' "'""") 
+print("""   AbmmmqMA     MM       MM     MM    MM  MM    MM    MM""")     
+print("""  A'     VML    MM       MM     MM    MM  MM    MM    MM""")     
+print(""".AMA.   .AMMA..JMML.     `Mbmo.JMML  JMML.`Mbod"YML..JMML.""")
+print("""                  Crated by Pietro Magni                       """)   
+                                                         
